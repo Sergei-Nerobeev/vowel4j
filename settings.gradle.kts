07e04b57f7df3c46807ee3c3d541e14651e5396f
coder <sergeinerobeev@gmail.com>
2025-08-15 19:18:03 +0200

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "vowel4j"
---
+++ src/main/java/hu/nerbe/Main.java
@@ -0,0 +1,13 @@
+package hu.nerbe;
+
+import static hu.nerbe.Letter.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println(!isVowelEn('a'));
+    System.out.println(isVowelRu('a'));
+    System.out.println(!isVowelHu('t'));
+  }
+
+}
