4e635961f71cf1167f7ce59abf20718c51aec4be
coder <sergeinerobeev@gmail.com>
2025-08-18 10:27:25 +0200

    cleaned master branch

--- src/main/java/hu/nerbe/Vowel4j.java
+++ src/main/java/hu/nerbe/Vowel4j.java
@@ -4,18 +4,5 @@ import java.util.Set;
 
 public class Vowel4j {
 
-  public static boolean isVowelRu(char ch) {
-    Set<Character> vowels = Set.of('а', 'е', 'ё', 'и', 'у', 'ы', 'э', 'ю', 'я');
-    return vowels.contains(ch);
-  }
 
-  public static boolean isVowelEn(char ch) {
-    Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'y');
-    return vowels.contains(ch);
-  }
-
-  public static boolean isVowelHu(char ch) {
-    Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'ö', 'u', 'ü', 'á', 'é', 'í', 'ó', 'ő', 'ú', 'ű');
-    return vowels.contains(ch);
-  }
 }
